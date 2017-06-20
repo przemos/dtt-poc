28460de08d7f7f90d6f82490bac11d08c98fb4b1
Grzegorz Krause <g.krause@kainos.com>
2017-06-20 11:23:37 +0200

    Clean-up for webcam component

--- client/app/components/Webcam.vue
+++ client/app/components/Webcam.vue
@@ -3,9 +3,6 @@
 
 		<video id="userWebcam" width="400" height="300" preload autoplay loop muted></video>
 		<canvas id="userCanvasId" width="400" height="300" ref="userCanvas"></canvas>
-		<h1 class="face-detection-warning" v-if="!faceFound">NO FACE DETECTED</h1>
-		<h1 class="face-detection-warning" v-if="multipleFacesFound">FOUND MULTIPLE FACES</h1>
-		<h1 id="noticeTimer"></h1>
 	</div>
 </template>
 <script>
@@ -13,7 +10,7 @@
 	import {face} from "tracking/build/data/face";
 
 	const WARNING_THRESHOLD_MS = 1500;
-	const FACE_DETECTION_TIMEOUT_MS = 6000;
+	const FACE_DETECTION_TIMEOUT_MS = 2500;
 
 	export default {
 
@@ -21,8 +18,6 @@
 			var self = this;
 			var canvas = self.$refs["userCanvas"];
 			var context = canvas.getContext("2d");
-
-			var noticeTimer = document.getElementById("noticeTimer");
 
 			var tracker = new tracking.ObjectTracker("face");
 			tracker.setInitialScale(4);
@@ -33,28 +28,25 @@
 
 			tracker.on('track', function(event) {
 				context.clearRect(0, 0, canvas.width, canvas.height);
-				if (event.data.length === 0) {
+				if (event.data.length === 0 || event.data.length > 1) {
 			    // No objects were detected in this frame.
 
 					if (self.lastTimeSeen + WARNING_THRESHOLD_MS < new Date().getTime()) {
-						self.faceFound = false;
-						self.multipleFacesFound = false;
-
-						noticeTimer.innerHTML = `TIME REMAINING ${Math.round(((FACE_DETECTION_TIMEOUT_MS + WARNING_THRESHOLD_MS - (new Date().getTime() - self.lastTimeSeen)) / 1000))}S`;
 
 						if (FACE_DETECTION_TIMEOUT_MS + WARNING_THRESHOLD_MS - (new Date().getTime() - self.lastTimeSeen) <= 0) {
-							self.$parent.$emit('potentialFraudFound', 'potentialFraudFound');
+							var potentialFraudData = {};
+							potentialFraudData.numberOfFacesFound = event.data.length;
+							potentialFraudData.eventTime = new Date().toLocaleString(); 
+
+							self.$parent.$emit('potentialFraudFound', potentialFraudData);
 							self.lastTimeSeen = new Date().getTime();
 						}						
 					}
-			  } else {
-					self.faceFound = true;
-					self.multipleFacesFound = event.data.length > 1;
+			  }
+				else {
 					self.lastTimeSeen = new Date().getTime();
 
-					noticeTimer.innerHTML = '';
-
-			    event.data.forEach(function(rect) {
+			    /*event.data.forEach(function(rect) {
 			      // rect.x, rect.y, rect.height, rect.width
 						context.strokeStyle = '#a64ceb';
 	          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
@@ -62,7 +54,7 @@
 	          context.fillStyle = "#fff";
 	          context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
 	          context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-		    });
+		    	});*/
 			  }
 			});
 		},
@@ -70,8 +62,6 @@
 		data() {
 			return {
 				now: null,
-				faceFound: true,
-				multipleFacesFound: false,
 				lastTimeSeen: new Date().getTime()
 			};
 		},
