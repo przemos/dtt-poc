cd5c5bddd0330b18b89074d6f18542f18dfa0a3c
Przemyslaw Sempruch <przemyslaws@kainos.com>
2017-06-22 16:02:24 +0200

    UX remarks

--- client/app/modules/result/index.vue
+++ client/app/modules/result/index.vue
@@ -1,53 +1,68 @@
 <template>
 	<div>
 		<header class="transaction-header" v-bind:class="[isPassed() ? 'passed' : 'failed']" style=" margin-bottom:50px">
-			<h1 class="transaction-header__title">Test complete</h1>
-			<p class="transaction-header__next-step">Please review your results</p>
+			<h1 class="transaction-header__title">{{outcomeText()}}</h1>
+			<p class="transaction-header__next-step">{{outcomeAuxText()}}</p>
 
 			<table class="c-transaction-details c-transaction-details--four">
 				<tbody>
 				<tr class="c-transaction-details__item c-transaction-details__item--first">
-					<th class="c-transaction-details__key">Person</th>
-					<td class="c-transaction-details__value">John Smith</td>
-				</tr>
-
-				<tr class="c-transaction-details__item">
-					<th class="c-transaction-details__key">Outcome</th>
-					<td class="c-transaction-details__value">{{outcomeText()}}</td>
-				</tr>
-
-				<tr class="c-transaction-details__item">
-					<th class="c-transaction-details__key">Score</th>
+					<th class="c-transaction-details__key">Your score</th>
 					<td class="c-transaction-details__value">{{scoreText}}</td>
 				</tr>
 
+				<tr class="c-transaction-details__item" style="text-align:center">
+					<th class="c-transaction-details__key">Outcome</th>
+					<td class="c-transaction-details__value">{{statusText()}}</td>
+				</tr>
+
 				<tr class="c-transaction-details__item c-transaction-details__item--last">
-					<th class="c-transaction-details__key">Test duration</th>
-					<td class="c-transaction-details__value">{{durationText}}</td>
+					<th class="c-transaction-details__key">Questions answered correctly</th>
+					<td class="c-transaction-details__value">{{correctlyAnsweredText}}</td>
 				</tr>
 				</tbody>
 			</table>
 		</header>
-		<div class="grid-row" align="center">
-			<div class="column-one-third">
-				<div class="data">
-					<span class="data-item bold-xxlarge">{{correctlyAnsweredText}}</span>
-					<span class="data-item bold-xsmall">Correct answers</span>
-				</div>
+
+		<div v-if="isPassed()">
+			<h2 class="heading-medium">What is next?</h2>
+			<div class="text">You need your certification number 21950/2017 when you book and take
+				your driving test.</div>
+
+			<div class="message--important">
+				Your pass certificate number lasts for 2 years. You must pass your
+				driving test in that time, otherwise you’ll have to pass the theory
+				test again.
 			</div>
-			<div class="column-one-third">
-				<div class="data">
-					<span class="data-item bold-xxlarge">{{leftUnansweredText}}</span>
-					<span class="data-item bold-xsmall">Left unanswered</span>
-				</div>
-			</div>
-			<div class="column-one-third">
-				<div class="data">
-					<span class="data-item bold-xxlarge">{{wrongText}}</span>
-					<span class="data-item bold-xsmall">Wrong answers</span>
-				</div>
-			</div>
+
+			<button class="button">Book your driving test</button>
+
+			<p><router-link to="/intro"><a  href="#" >Back to home</a></router-link></p>
 		</div>
+
+
+	<div v-if="!isPassed()">
+		<h2 class="heading-medium">What is next?</h2>
+		<div class="text">
+			You must book and take the full test again, even if you passed one part this time.
+			<br/><br/>
+			<details>
+				<summary role="button" aria-controls="details-content-0" tabindex="0" aria-expanded="false"><span class="summary">See you performance by topic for this test</span></summary>
+				<div class="panel-indent" id="details-content-0" aria-hidden="true" style="display: none;">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu lorem dolor. Fusce et mauris augue. Aenean eu feugiat nibh, nec volutpat metus.</p>
+				</div>
+			</details>
+			<br/>
+			<p>You have to wait at least 3 working days before taking your test again.</p>
+		</div>
+		<p>In the meantime you can <a href="#">take a mock theory test</a>.</p>
+
+
+		<button class="button">Book your driving test</button>
+
+		<p><router-link to="/intro"><a  href="#" >Back to home</a></router-link></p>
+	</div>
+
 		<div v-if="fraudDetected()">
 			<div class="message--failure">
 				<h3 class="message__heading" style="margin : 0;">Cheat markers detected</h3>
@@ -55,7 +70,6 @@
 				<p v-if="isMoreThanOneFaceAppeared()">More than one face detected in the camera viewport</p>
 				<p v-if="isFaceKeptOffReason()">Face kept off camera viewport for too long</p>
 			</div>
-
 		</div>
 	</div>
 
@@ -94,7 +108,7 @@
 				return Math.floor(this.$store.getters.correctlyAnsweredCount / this.$store.getters.totalCount * 100) + "%";
 			},
 			correctlyAnsweredText: function () {
-				return this.$store.getters.correctlyAnsweredCount;
+				return this.$store.getters.correctlyAnsweredCount + " out of " +  this.$store.getters.totalCount;
 			},
 			leftUnansweredText: function () {
 				return this.$store.getters.unansweredCount;
@@ -105,10 +119,10 @@
 		},
 		methods: {
 			isPassed: function () {
-				return this.$store.getters.correctlyAnsweredCount === 3 && !this.fraudDetected();
+				return this.$store.getters.correctlyAnsweredCount === 3;
 			},
-			outcomeText: function () {
-				return this.isPassed() ? "Passed" : "Failed";
+			statusText: function () {
+				return this.isPassed() ? "Pass" : "Fail";
 			},
 			fraudDetected: function () {
 				return this.isFaceKeptOffReason() || this.isMoreThanOneFaceAppeared();
@@ -119,14 +133,23 @@
 			isMoreThanOneFaceAppeared: function () {
 				return this.$store.getters.numberOfTimesMoreThanOneFaceAppeared > 1;
 			},
+			outcomeText : function () {
+				return this.isPassed() ? 'Theory Test completed' : 'Theory Test failed';
+			},
+			outcomeAuxText : function () {
+			    return this.isPassed() ? 'Your pass certificate number is 21950/2017' : 'You must book and take the full test again';
+			}
 		}
 	};
 
 </script>
 <style>
 
+	.cheated {
+		background-color: #005ea5;
+	}
 	.passed {
-		background-color: darkgreen;
+		background-color: #28a197;
 	}
 
 	.failed {
--- client/app/modules/test/intro.vue
+++ client/app/modules/test/intro.vue
@@ -7,14 +7,22 @@
 		</div>
 
 		<div class="message">
-			<p>To attend, make sure that your camera is switched on</p>
+			<div class="message__heading">This test is supervised</div>
+				To take part, make sure that your camera is switched on.
 		</div>
 
 		<p>Before you start, you need to set up your camera.</p>
 
-
+		<br/>
+		<p>There are 2 parts to the test</p>
+		<ul class="list-bullet">
+			<li>multiple-choice questions</li>
+			<li>hazard perception - a video test about spotting hazards on the road</li>
+		</ul>
+		<p>You book and take them as a single test. You must pass both parts to pass the
+			test.</p>
 		<p>
-			<router-link to="/calibration"><a class="button" href="#" role="button">Continue</a></router-link>
+			<router-link to="/calibration"><a class="button" href="#" role="button">Set up your camera</a></router-link>
 			<!--<router-link to="/test"><a class="button" href="#" role="button">[DEV] Skip calibration</a></router-link>-->
 		</p>
 	</div>
